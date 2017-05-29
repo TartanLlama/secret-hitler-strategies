17a397141704d99512e7a4bac2a9886957e8e079
Simon Brand <simon@codeplay.com>
2017-05-29 15:33:52 +0100

    Add table of contents. Fixes #2.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,12 @@
 
 $( document ).ready(function() {
 
+    $("hr").first().after('<h2>Table of Contents</h2><ul id="table-of-contents"></ul><hr/>')
+    
     $("h2").filter("[id]").each(function () {
-        $(this).append('<a href="#' + $(this).attr("id") + '"><i class="fa fa-link"></i></a>');
+        var linkText = '<a href="#' + $(this).attr("id") + '">'
+        $(this).append(linkText + '<i class="fa fa-link"></i></a>');
+        $('#table-of-contents').append('<li>' + linkText + $(this).contents().text() + '</a></li>');
     });
 
 });
