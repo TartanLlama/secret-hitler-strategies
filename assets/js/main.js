bbe5402d89bcd077e8713163dcf18e21b3d3a983
Simon Brand <tartanllama@gmail.com>
2017-06-05 22:47:39 +0100

    Add section links back in and add them for h3 and h4 elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,8 @@
 ---
 
 $( document ).ready(function() {
+    $("h2,h3,h4").filter("[id]").each(function () {
+        var linkText = '<a href="#' + $(this).attr("id") + '">'
+        $(this).append(linkText + '<i class="fa fa-link"></i></a>');
+    });
 });
